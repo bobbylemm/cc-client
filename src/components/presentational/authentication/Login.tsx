054d84e2b054c0e78234c299699245bd132f9ce1
bobbylemm <chidozienwoga@gmail.com>
2021-04-03 08:43:11 +0100

    add login form

--- src/components/presentational/authentication/Login.tsx
+++ src/components/presentational/authentication/Login.tsx
@@ -1,10 +1,69 @@
-import React from 'react'
-import { Heading } from "@chakra-ui/react"
-
+import React from "react";
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Link,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
 interface LoginViewProps {}
 
 const LoginView: React.FC<LoginViewProps> = () => {
-    return <Heading>Login page view</Heading>
-}
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading textTransform="capitalize" fontSize={"4xl"}>
+            Sign in to your account
+          </Heading>
+          <Text fontSize={"lg"} color={"gray.600"}>
+            to view and place orders
+          </Text>
+        </Stack>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Stack spacing={4}>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password" />
+            </FormControl>
+            <Stack spacing={5}>
+              <Box />
+              <Button
+                bg={"blue.400"}
+                color={"white"}
+                _hover={{
+                  bg: "blue.500",
+                }}
+              >
+                Sign in
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+};
 
-export default LoginView
+export default LoginView;
